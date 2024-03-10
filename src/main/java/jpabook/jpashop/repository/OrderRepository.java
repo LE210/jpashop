584f283fa759154f6ccfd83728fb89666c46d05b
le210 <kapi_bm@naver.com>
2024-03-11 01:58:18 +0900

    Order 서비스 repo 개발 및 테스트

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
@@ -19,7 +21,12 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
     // 검색용
-//    public List<Order> findAll() {
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m where o.status = :status and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+//                .setFirstResult(100) // 페이징
+                .setMaxResults(1000)// 최대 1000건
+                .getResultList();
+    }
 }
